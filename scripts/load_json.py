9bae90b0cab8db48d62e19dc137df8c3fca179f2
Tom Webber <thomas.webber@digital.justice.gov.uk>
2024-11-12 16:59:53 +0000

    feat: update load_json.py: iterate over pathlist; only run from correct account
    also adds .env.template to populate with account numbers

--- scripts/load_json.py
+++ scripts/load_json.py
@@ -1,45 +1,54 @@
-import boto3
+import os
 import pickle
+from pathlib import Path
 
-glue_client = boto3.client("glue", "eu-west-1")
+import boto3
+from dotenv import find_dotenv, load_dotenv
 
-with open("delius_borough_table_json_121124.pkl", "rb") as f:
-    json_data = pickle.load(f)
+load_dotenv(find_dotenv())
 
-table = json_data['Table']
+glue_client = boto3.client("glue", region_name="eu-west-1")
 
-source_bucket = "mojap-derived-tables"
-dest_bucket = "mojap-compute-development-derived-tables-replication"
+session = boto3.Session()
+sts_client = session.client("sts")
+account_id = sts_client.get_caller_identity()["Account"]
+expected_account_name = "analytical-platform-compute-development"
+expected_account_id = os.environ.get(expected_account_name)
 
-# table['StorageDescriptor']['Location'] = (
-#     table['StorageDescriptor']['Location']
-#     .replace(source_bucket, dest_bucket)
-# )
+if account_id != expected_account_id:
+    raise ValueError(f"Not running in expected account: {expected_account_name}")
 
-# if 'metadata_location' in table['Parameters']:
-#     table['Parameters']['metadata_location'] = (
-#         table['Parameters']['metadata_location']
-#         .replace(source_bucket, dest_bucket)
-#     )
+directory = "json_output"
+pathlist = Path(directory).glob("**/*.pkl")
 
-print(table)
+for path in pathlist:
+    path_in_str = str(path)
 
-try:
-    glue_client.create_table(
-                DatabaseName=table['DatabaseName'],
-                TableInput={
-                    'Name': table['Name'],
-                    'Description': table.get('Description', ''),
-                    'StorageDescriptor': table['StorageDescriptor'],
-                    'PartitionKeys': table.get('PartitionKeys', []),
-                    'TableType': table.get('TableType', 'EXTERNAL_TABLE'),
-                    'Parameters': table.get('Parameters', {}),
-                    # 'LastAccessTime': table.get('LastAccessTime'),
-                    'Retention': table.get('Retention', 0)
-                }
-            )
-    print("table created")
-except Exception as e:
-    print("table creation failed:", e)
+    with open(path_in_str, "rb") as file_name:
+        json_data = pickle.load(file_name)
+
+    table = json_data["Table"]
+
+    source_bucket = "mojap-derived-tables"
+
+    print(table)
+
+    try:
+        glue_client.create_table(
+            DatabaseName=table["DatabaseName"],
+            TableInput={
+                "Name": table["Name"],
+                "Description": table.get("Description", ""),
+                "StorageDescriptor": table["StorageDescriptor"],
+                "PartitionKeys": table.get("PartitionKeys", []),
+                "TableType": table.get("TableType", "EXTERNAL_TABLE"),
+                "Parameters": table.get("Parameters", {}),
+                # 'LastAccessTime': table.get('LastAccessTime'),
+                "Retention": table.get("Retention", 0),
+            },
+        )
+        print("table created")
+    except Exception as e:
+        print("table creation failed:", e)
 
 # Add MSCK REPAIR TABLE statement after table creation
